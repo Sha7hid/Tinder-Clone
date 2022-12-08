4ec6a189ccc34492c61c095187253956c1d18c0b
sha7hid <shahidshafadshahad@gmail.com>
2022-12-08 19:23:00 +0530

    updt

--- tinder-clone/src/SwipeButtons.jsx
+++ tinder-clone/src/SwipeButtons.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import './SwipeButtons.css';
+import ReplayIcon from '@mui/icons-material/Replay';
+import CloseIcon from '@mui/icons-material/Close';
+import StarRateIcon from '@mui/icons-material/StarRate';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
+import IconButton from '@mui/material/IconButton';
 function SwipeButtons() {
   return (
-    <div>
+    <div className='swipeButtons'>
       
     </div>
   )
