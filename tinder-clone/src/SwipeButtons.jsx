66a83e59c8c87691dc15b8794c3bb22e91a5fced
sha7hid <shahidshafadshahad@gmail.com>
2022-12-08 19:53:57 +0530

    updt

--- tinder-clone/src/SwipeButtons.jsx
+++ tinder-clone/src/SwipeButtons.jsx
@@ -9,7 +9,21 @@ import IconButton from '@mui/material/IconButton';
 function SwipeButtons() {
   return (
     <div className='swipeButtons'>
-      
+      <IconButton className='swipeButtons__repeat'>
+        <ReplayIcon fontSize='large'/>
+      </IconButton>
+      <IconButton className='swipeButtons__left'>
+        <CloseIcon fontSize='large'/>
+      </IconButton>
+      <IconButton className='swipeButtons__star'>
+        <StarRateIcon fontSize='large'/>
+      </IconButton>
+      <IconButton className='swipeButtons__right'>
+        <FavoriteIcon fontSize='large'/>
+      </IconButton>
+      <IconButton className='swipeButtons__lightning'>
+        <FlashOnIcon fontSize='large'/>
+      </IconButton>
     </div>
   )
 }
